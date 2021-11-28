0febe31a3b50b6431527efec945314d894317d19
VoN <sudo.von.contact@gmail.com>
2021-11-27 22:13:28 -0600

    feat: Add overload

---
+++ Overload/Overload/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sum(1,2));
+            Console.WriteLine(sum(1,2,3));
+            Console.WriteLine(sum(1,2,3,4));
+        }
+
+        static int sum(int num1, int num2) => num1 + num2;
+        static int sum(int num1, int num2, int num3) => num1 + num2 + num3;
+        static int sum(int num1, int num2, int num3, int num4) => num1 + num2 + num3 + num4;
+    }
+}
