00091d6766a06bc244809f9acff6a120b380a576
VoN <sudo.von.contact@gmail.com>
2021-11-28 00:56:10 -0600

    feat: Add switch

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Your number is 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Your number is 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Your number is 3");
+                    break;
+                default:
+                    Console.WriteLine("Your number is bigger than 3");
+                    break;
+            }
+        }
+    }
+}
