523ab02d67fef7ba25aa79e8782f9f8e3a36e26c
VoN <sudo.von.contact@gmail.com>
2021-11-27 19:35:51 -0600

    feature: Add parsing

---
+++ Parsing/Parsing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Parsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First number");
+            int numberOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Second number");
+            int numberTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The result from {numberOne} + {numberTwo} is {numberOne + numberTwo}");
+
+        }
+    }
+}
