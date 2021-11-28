da879aee32846dda73b758bb92f5c225fc2fbf88
VoN <sudo.von.contact@gmail.com>
2021-11-27 19:41:10 -0600

    feat: Add constants

---
+++ Constants/Constants/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Not allowed: const int NUMBER; */
+            const int NUMBER = 50;
+            Console.WriteLine($"Constant's value is: {NUMBER}");
+        }
+    }
+}
