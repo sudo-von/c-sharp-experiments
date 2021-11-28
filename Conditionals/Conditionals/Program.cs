e80335cd52d341e12c203a7d8a728a6029c448ab
VoN <sudo.von.contact@gmail.com>
2021-11-28 00:43:20 -0600

    feat: Add conditionals

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCold = false;
+            if (isCold)
+            {
+                Console.WriteLine("It's cold");
+            }
+            else
+            {
+                Console.WriteLine("It's not cold");
+            }
+        }
+    }
+}
