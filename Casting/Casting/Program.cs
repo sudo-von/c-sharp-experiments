515584092a16849c688bfd7e3c2e9e95495e3fe2
VoN <sudo.von.contact@gmail.com>
2021-11-27 19:29:48 -0600

    feature: Add casting

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Explicit */
+            double temperature = 35.0;
+            int temperature2 = (int)temperature;
+            /* 
+             * Implicit 
+             * https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/numeric-conversions
+             */
+            int people = 10000;
+            long people2 = people;
+            float weight = 5.78F;
+            double weight2 = weight;
+        }
+    }
+}
