8120026a752e94611801ac30c743a49109727642
VoN <sudo.von.contact@gmail.com>
2021-11-28 20:24:28 -0600

    feat: Add pooexamples

---
+++ POOExamples/POOExamples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace POOExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Creation. */
+            Circle myCircle;
+            /* Instance. */
+            myCircle = new Circle();
+            double area = myCircle.calculateArea(5);
+            Console.WriteLine($"The area is: {area}");
+
+            Circle myCircleTwo = new Circle();
+            double areaTwo = myCircle.calculateArea(6);
+            Console.WriteLine($"The second area is: {areaTwo}");
+        }
+    }
+
+    class Circle
+    {
+        const double PI = 3.1416;
+        public double calculateArea(int radius)
+        {
+            return PI * radius * radius;
+        }
+    }
+}
