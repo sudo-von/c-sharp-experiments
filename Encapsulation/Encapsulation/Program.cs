a32011ad3e23d44d03636cdd018d90f332c0550c
VoN <sudo.von.contact@gmail.com>
2021-11-28 22:24:26 -0600

    feat: Add encapsulation

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConverterEuroDollar conv = new ConverterEuroDollar();
+            Console.WriteLine(conv.convert(50));
+            /* conv.euro = 1.45; */
+            conv.changeEuroValue(1.45);
+            Console.WriteLine(conv.convert(50));
+        }
+    }
+
+    class ConverterEuroDollar
+    {
+        private double euro = 1.253;
+
+        public double convert(double quantity)
+        {
+            return quantity * euro;
+        }
+
+        public void changeEuroValue(double newValue)
+        {
+            if (newValue > 0)
+            {
+                euro = newValue;
+            }
+        }
+    }
+
+}
