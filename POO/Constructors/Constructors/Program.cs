0aeb2b151fceb8e0e173fdbf74507a9ad869d947
VoN <sudo.von.contact@gmail.com>
2021-12-05 20:16:23 -0600

    feature: add constructors

---
+++ POO/Constructors/Constructors/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car firstCar = new Car();
+            Console.WriteLine($"First car's length: {firstCar.getLength()}");
+            Console.WriteLine($"First car's width: {firstCar.getWidth()}");
+            Car secondCar = new Car(4600.10,.1600);
+            Console.WriteLine($"Second car's length: {secondCar.getLength()}");
+            Console.WriteLine($"Second car's width: {secondCar.getWidth()}");
+        }
+    }
+
+    class Car
+    {
+        private int wheels;
+        private double length;
+        private double width;
+        public Car()
+        {
+            wheels = 4;
+            length = 2300.5;
+            width = 0.800;
+        }
+        public Car(double lengthCar, double widthCar)
+        {
+            wheels = 4;
+            length = lengthCar;
+            width = widthCar;
+        }
+        public double getLength()
+        {
+            return length;
+        }
+        public double getWidth()
+        {
+            return width;
+        }
+    }
+}
