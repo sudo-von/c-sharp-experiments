1c1e548be6607ff782dec1d1750f2b58e6d352f4
VoN <sudo.von.contact@gmail.com>
2021-12-05 21:15:32 -0600

    feat: Add POOConcepts

---
+++ POO/POOConcepts/POOConcepts/Dot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POOConcepts
+{
+    class Dot
+    {
+        public Dot()
+        {
+            Console.WriteLine("Default constructor...");
+        }
+        public Dot(int x, int y)
+        {
+            Console.WriteLine($"x: {x}\ny: {y}");
+        }
+    }
+}
---
+++ POO/POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace POOConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Check to do list.
+            doWork();
+        }
+
+        static void doWork()
+        {
+            Dot origin = new Dot();
+            Dot destination = new Dot(10,20);
+        }
+    }
+}
