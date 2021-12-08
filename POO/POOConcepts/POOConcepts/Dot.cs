4a07373a2a7c6fc743c3fefc6238b0c67c37ca53
VoN <sudo.von.contact@gmail.com>
2021-12-07 21:42:26 -0600

    feat: add more concepts

--- POO/POOConcepts/POOConcepts/Dot.cs
+++ POO/POOConcepts/POOConcepts/Dot.cs
@@ -6,13 +6,25 @@ namespace POOConcepts
 {
     class Dot
     {
-        public Dot()
-        {
-            Console.WriteLine("Default constructor...");
-        }
         public Dot(int x, int y)
         {
-            Console.WriteLine($"x: {x}\ny: {y}");
+            this.x = x;
+            this.y = y;
         }
+        public Dot()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        public double getDistance(Dot anotherDot)
+        {
+            int xDif = this.x - anotherDot.x;
+            int yDif = this.y - anotherDot.y;
+            double distance = Math.Sqrt(Math.Pow(xDif,2) + Math.Pow(yDif,2));
+            return distance;
+        }
+
+        private int x, y;
     }
 }
--- POO/POOConcepts/POOConcepts/Program.cs
+++ POO/POOConcepts/POOConcepts/Program.cs
@@ -14,6 +14,8 @@ namespace POOConcepts
         {
             Dot origin = new Dot();
             Dot destination = new Dot(10,20);
+            double distance = origin.getDistance(destination);
+            Console.WriteLine(distance);
         }
     }
 }
