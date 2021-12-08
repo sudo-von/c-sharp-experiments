87e011d877aa5ee27fd1ff363b802d0b70af8e62
VoN <sudo.von.contact@gmail.com>
2021-12-08 10:09:09 -0600

    feat: Add loops

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new[]
+            {
+                new { Name = "Sudo", Age = 24},
+                new { Name = "VoN", Age = 24}
+            };
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.WriteLine($"name: {people[i].Name} - age: {people[i].Age}");
+            }
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"name: {person.Name} - age: {person.Age}");
+            }
+
+        }
+    }
+}
